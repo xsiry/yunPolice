f52ae19e83b2a6b8d1a6d48c541d9e05d7b4e82e
xsiry <xsiries@gmail.com>
2017-07-31 20:56:31 +0800

    #案件数据统计后台

---
+++ static/case_statistics_mgmt/src/columns.js
@@ -0,0 +1,186 @@
+define([{
+  display: '案发辖区',
+  name: 'areaname',
+  isSort: false,
+  minWidth: 100
+}, {
+  display: '除主动管理类案件',
+  minWidth: 80,
+  isSort: false,
+  columns: [{
+    display: '本期',
+    name: 'noActivemgmtCurrent',
+    isSort: false
+  }, {
+    display: '同期',
+    name: 'noActivemgmtEarlier',
+    isSort: false
+  }, {
+    display: '同比',
+    name: 'noActivemgmtRatio',
+    isSort: false
+  }]
+}, {
+  display: '三降案件',
+  minWidth: 80,
+  isSort: false,
+  columns: [{
+    display: '现行案件总数',
+    isSort: false,
+    columns: [{
+      display: '本期',
+      name: 'currentCaseCurrent',
+      isSort: false
+    }, {
+      display: '同期',
+      name: 'currentCaseEarlier',
+      isSort: false
+    }, {
+      display: '同比',
+      name: 'currentCaseRatio',
+      isSort: false
+    }]
+  }, {
+    display: '盗窃',
+    isSort: false,
+    columns: [{
+      display: '入室盗窃',
+      isSort: false,
+      columns: [{
+        display: '本期',
+        name: 'housebreakingCurrent',
+        isSort: false
+      }, {
+        display: '同期',
+        name: 'housebreakingEarlier',
+        isSort: false
+      }, {
+        display: '同比',
+        name: 'housebreakingRatio',
+        isSort: false
+      }]
+    }, {
+      display: '盗窃总发案',
+      isSort: false,
+      columns: [{
+        display: '本期',
+        name: 'larcenyCurrent',
+        isSort: false
+      }, {
+        display: '同期',
+        name: 'larcenyEarlier',
+        isSort: false
+      }, {
+        display: '同比',
+        name: 'larcenyRatio',
+        isSort: false
+      }]
+    }]
+  }, {
+    display: '诈骗',
+    isSort: false,
+    columns: [{
+      display: '电信诈骗',
+      isSort: false,
+      columns: [{
+        display: '本期',
+        name: 'telecomfraudCurrent',
+        isSort: false
+      }, {
+        display: '同期',
+        name: 'telecomfraudEarlier',
+        isSort: false
+      }, {
+        display: '同比',
+        name: 'telecomfraudRatio',
+        isSort: false
+      }]
+    }, {
+      display: '诈骗总发案',
+      isSort: false,
+      columns: [{
+        display: '本期',
+        name: 'fraudCurrent',
+        isSort: false
+      }, {
+        display: '同期',
+        name: 'fraudEarlier',
+        isSort: false
+      }, {
+        display: '同比',
+        name: 'fraudRatio',
+        isSort: false
+      }]
+    }]
+  }]
+}, {
+  display: '两抢案件',
+  minWidth: 80,
+  isSort: false,
+  columns: [{
+    display: '本期',
+    name: 'twoRobCurrent',
+    isSort: false
+  }, {
+    display: '同期',
+    name: 'twoRobEarlier',
+    isSort: false
+  }, {
+    display: '同比',
+    name: 'twoRobRatio',
+    isSort: false
+  }]
+}, {
+  display: '主动管理类案件',
+  minWidth: 80,
+  isSort: false,
+  columns: [{
+    display: '毒品案件',
+    isSort: false,
+    columns: [{
+      display: '本期',
+      name: 'hardrugCurrent',
+      isSort: false
+    }, {
+      display: '同期',
+      name: 'hardrugEarlier',
+      isSort: false
+    }, {
+      display: '同比',
+      name: 'hardrugRatio',
+      isSort: false
+    }]
+  }, {
+    display: '治安类刑事案件',
+    isSort: false,
+    columns: [{
+      display: '本期',
+      name: 'criminalJusticeCurrent',
+      isSort: false
+    }, {
+      display: '同期',
+      name: 'criminalJusticeEarlier',
+      isSort: false
+    }, {
+      display: '同比',
+      name: 'criminalJusticeRatio',
+      isSort: false
+    }]
+  }, {
+    display: '经济案件',
+    isSort: false,
+    columns: [{
+      display: '本期',
+      name: 'economicCurrent',
+      isSort: false
+    }, {
+      display: '同期',
+      name: 'economicEarlier',
+      isSort: false
+    }, {
+      display: '同比',
+      name: 'economicRatio',
+      isSort: false
+    }]
+  }]
+}])
---
+++ static/case_statistics_mgmt/src/module.js
@@ -0,0 +1,170 @@
+define(function(require, exports, module) {
+  $.root_ = $('div.ibox-content');
+  var manager, g;
+  var domain = "/didiweb/";
+  module.exports = {
+
+    init: function() {
+      datetimepickerInit();
+      f_initGrid();
+      this._configText();
+      this._bindUI();
+    },
+    _configText() {
+      $('div h5.mgmt_title').text('案件统计列表');
+      $('div button font.mgmt_export_btn').text('导出');
+      $('div input.name_search').prop('placeholder', '输入咨询标题');
+      $('div button font.mgmt_search_btn').text('搜索');
+    },
+    _bindUI: function() {
+      // bind .name_search_btn
+      $.root_.on("click", '.search_btn', function(e) {
+        manager.setOptions({
+            parms: {
+              startedat: $('#startedat').val()+':00',
+              endedat: $('#endedat').val()+':59'
+            }
+        });
+        //将会覆盖设置表格对象属性时的parms属性
+
+        //查询时，设置为第一页！！
+        manager.changePage("first");
+
+        //重新发起ajax请求，加载数据
+        manager.loadData(true);
+      })
+        // bind .name_search
+      $.root_.on("keypress", '.name_search', function(e) {
+        if (e.which == "13") f_search();
+      })
+        // bind .name_search val.length is 0
+      $.root_.on('input propertychange', '.name_search', function(e) {
+        if ($('.name_search').val().length == 0) f_search();
+      })
+      $.root_.on("click", '.export_btn', function(e) {
+        t_export();
+      })
+    }
+  };
+
+  // Helpers
+  /*
+   * 生成Grid
+   */
+  function f_initGrid() {
+    var c = require('./columns');
+    g = manager = $("div.listDiv").ligerGrid({
+      columns: c,
+      onSelectRow: function(rowdata, rowindex) {
+        $("#txtrowindex").val(rowindex);
+      },
+      url: domain + 'case_statistics/getList.do',
+      parms: {
+        startedat: $('#startedat').val()+':00',
+        endedat: $('#endedat').val()+':59'
+      },
+      method: "get",
+      dataAction: 'server',
+      usePager: true,
+      enabledEdit: false,
+      clickToEdit: false,
+      width: '100%',
+      height: '91%'
+    });
+  };
+
+  /*
+   * 导出
+   */
+  function t_export() {
+    window.open(domain + 'case_statistics/xExport.do');
+  }
+
+  /*
+   * 搜索
+   */
+  function f_search() {
+    g.options.data = $.extend(true, {}, gridData);
+    g.loadData(f_getWhere());
+  };
+
+  function f_getWhere() {
+    if (!g) return null;
+    var clause = function(rowdata, rowindex) {
+      var key = $(".name_search").val();
+      return rowdata.tit.indexOf(key) > -1;
+    };
+    return clause;
+  };
+
+  function datetimepickerInit() {
+    var start = $("#startedat").datetimepicker({
+      format: "yyyy-mm-dd hh:ii",
+          autoclose: true,
+          todayBtn: true,
+          minuteStep: 1,
+          language: 'zh-CN'
+    });
+
+    var end = $("#endedat").datetimepicker({
+      format: "yyyy-mm-dd hh:ii",
+          autoclose: true,
+          todayBtn: true,
+          minuteStep: 1,
+          language: 'zh-CN'
+    });
+
+    $('#startedat').val(genNowDate(true));
+    $('#endedat').val(genNowDate(false));
+
+    start.on('changeDate', function(e){
+      end.datetimepicker('setStartDate', e.date);
+    });
+    end.on('changeDate', function(e){
+        start.datetimepicker('setEndDate', e.date);
+    });
+  }
+
+    /* 获取当天时间 */
+  function genNowDate(bool) {
+    var dateTime = new Date();
+
+    function p(s) {
+      return s < 10 ? '0' + s : s;
+    }
+    //获取当前年
+    var year = dateTime.getFullYear();
+    //获取当前月
+    var month = dateTime.getMonth() + 1;
+    //获取当前日
+    var date = dateTime.getDate();
+    var now = [year, p(month), p(date)].join('-') + " " + (bool ? "00:00" : "23:59");
+    return now;
+  }
+
+
+  function dateFactory(str, date, yearBool) {
+    function p(s) {
+      return s < 10 ? '0' + s : s;
+    }
+
+    var d = date ? date : string2date(str);
+    //获取当前年
+    var year = d.getFullYear();
+    //获取当前月
+    var month = d.getMonth() + 1;
+    //获取当前日
+    var date = d.getDate();
+
+    var h = d.getHours(); //获取当前小时数(0-23)
+    var m = d.getMinutes(); //获取当前分钟数(0-59)
+    var s = d.getSeconds();
+    var mydatetime = yearBool ? [[year, p(month), p(date)], [p(h), p(m), p(s)]] : [p(month), p(date)];
+    var now = yearBool ?  [mydatetime[0].join('-'),mydatetime[1].join('-')].join(' ') : mydatetime.join('-');
+    return now;
+  }
+
+  function string2date(str) {
+    return new Date(Date.parse(str.replace(/-/g, "/")) -24*60*60*1000);
+  }
+})
